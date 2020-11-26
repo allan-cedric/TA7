ae3416dd670044f181c96a5fd9684aaa4f4acf09
Gabriel Nascarella <ielnascarella@hotmail.com>
2020-11-26 08:49:12 -0300

    docs: Adiciona comentarios

--- takePictures.py
+++ takePictures.py
@@ -1,18 +1,26 @@
 #!/usr/bin/env python3
 import cv2
 
+# Conecta com a webcam:
 capture = cv2.VideoCapture(0)
-cv2.namedWindow('Window', cv2.WINDOW_NORMAL)
 
+# Retorno da leitura da camera:
 success = True
-count = 1
-while success:
-    success, image = capture.read()
-    if success:
 
+# Contagem de fotos tiradas
+count = 1
+
+while success:
+    # Le a imagem da camera
+    success, image = capture.read()
+    
+    if success:
+        # Mostra a imagem
         cv2.imshow("Window", image)
+        # Recebe o input dado
         key=cv2.waitKey(5) & 0xFF
 
         if chr(key) == 's':
+            # Salva a imagem
             cv2.imwrite("pictures/chessboard{}.png".format(count), image)
             count += 1
